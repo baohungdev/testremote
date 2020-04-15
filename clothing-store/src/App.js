fbded1010a6d596de3f5c5c412501097895e203a
baohungdev <baohungdev@icloud.com>
2020-04-15 09:10:47 +0700

    breaken our hompages into smaller components, finish styling our menuitem

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './hompage.components';
 
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
